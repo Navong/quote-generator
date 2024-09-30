6674e2309e820e7e67c59643fc40f282d405059d
navong <navongbcvbong@gmail.com>
2024-10-01 05:06:29 +0900

    config

--- server/index.js
+++ server/index.js
@@ -119,7 +119,7 @@ app.delete('/api/favorites/:id', async (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 4002;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
